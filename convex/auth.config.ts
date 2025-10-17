b21bf63db00a667752c230ca09f9d985e5bcfa40
navadeep15 <navadeepchv@gmail.com>
2025-10-17 12:55:57 +0530

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://normal-pup-86.clerk.accounts.dev",//clerk front end api 
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+function page(){
+    console.log("He hi ");
+    return <div>About</div>
+    
+}
+
+export default page;
